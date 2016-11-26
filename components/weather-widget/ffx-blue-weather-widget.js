f05be7c2fdd5725d2475ffd53d3b7b026860c519
Roger Ng <roger.ng@helloworld.com.au>
2016-11-26 23:39:43 +1100

    // for weather url

--- components/weather-widget/ffx-blue-weather-widget.js
+++ components/weather-widget/ffx-blue-weather-widget.js
@@ -13,7 +13,7 @@ export default class FfxBlueWeatherWidget {
   constructor(element, dynamicOptions) {
     const defaults = {
       weatherApiKey: '0662751880d435bb226a86660788e48c',
-      weatherUrl: 'http://api.openweathermap.org/data/2.5/weather',
+      weatherUrl: '//api.openweathermap.org/data/2.5/weather',
       fallBackLocation: 'Sydney',
       units: 'metric',
       wind: true
