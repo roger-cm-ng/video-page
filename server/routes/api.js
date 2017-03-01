f8e2d62989b6faf93ccaa7b45a02ff24dd5927a2
Roger Ng <roger.ng@helloworld.com.au>
2017-03-01 14:55:11 +1100

    nodemon added

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,49 +1,14 @@
 import express from 'express';
-import mongoose from 'mongoose'
+import cors from 'cors';
 const Api = express.Router();
-const Schema = mongoose.Schema;
-const mongodbUri = 'mongodb://ffx:ffx123@ds111748.mlab.com:11748/heroku_4fv3kgzm';
+Api.use(cors());
 
-mongoose.connect(mongodbUri);
-
-const widgetSchema = new Schema({
-  title: String,
-  fallBackLocation: String,
-  unit: String,
-  wind: Boolean,
-  created: Date
-});
-
-const Widgets = mongoose.model('widgets', widgetSchema);
-
-Api.get('/get-all', (req, res) => {
-  Widgets.find()
-    .then((results) => {
-      res.json({
-        status: 200,
-        results
-      });
-    })
-});
-
-Api.post('/insert', (req, res) => {
-  const item = {
-    title: req.body.title,
-    fallBackLocation: req.body.fallBackLocation,
-    unit: req.body.unit,
-    wind: req.body.wind,
-    created: new Date()
-  }
-
-  const data = new Widgets(item);
-  data.save(() => {
-    Widgets.find()
-      .then((results) => {
-        res.json({
-          status: 200,
-          results
-        });
-      })
+Api.get('/get-banners', (req, res) => {
+  res.json({
+    status: 200,
+    data: {
+      result: { foo: 'bar' }
+    }
   });
 });
 
