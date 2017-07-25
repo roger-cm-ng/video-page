db07b1832f9555daba689ac55cbb4318c98b2a10
Garney Andres <garney.andres@3plearning.com>
2017-07-25 15:14:28 +1000

    add 3p-resource

--- components/reload/entry-reload.js
+++ components/reload/entry-reload.js
@@ -2,16 +2,24 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import thunk from 'redux-thunk';
+import environment from '3p-resource';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { handleDefaults } from '../helpers/utils';
 import ReloadCombinedReducers from './reload-combined-reducers';
 import Reload from './reload';
 
+
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
 export default class EntryReload {
 	constructor(element, dynamicOptions) {
+		if (window.pppAppConfig) {
+			const config = window.pppAppConfig;
+			environment(config.env || 'live')
+				.setConfiguration(config);
+		}
+
 		const defaults = {};
 		this.element = element;
 		this.options = handleDefaults(defaults, dynamicOptions);
