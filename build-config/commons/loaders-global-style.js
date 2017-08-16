2f707e6e45e41ff54bc44eaffdf7afa8353c330f
Ruofan Yin <ruofan.yin@3plearning.com>
2017-08-16 13:55:21 +1000

    remove unnecessary debugging message

--- build-config/commons/loaders-global-style.js
+++ build-config/commons/loaders-global-style.js
@@ -2,24 +2,22 @@ var path = require('path');
 
 module.exports = function exports() {
     var test = path.resolve('styles');
-    console.log('test: ', test);
-
-  return {
-    test: /\.(scss)$/,
-    include: [ path.resolve('styles') ],
-    loaders: [
-        {
-            loader: 'style-loader'
-        },
-        {
-            loader: 'css-loader?modules=false'
-        },
-        {
-            loader: 'postcss-loader'
-        },
-        {
-            loader: 'sass-loader',
-        }
-    ]
-  };
+    return {
+        test: /\.(scss)$/,
+        include: [path.resolve('styles')],
+        loaders: [
+            {
+                loader: 'style-loader'
+            },
+            {
+                loader: 'css-loader?modules=false'
+            },
+            {
+                loader: 'postcss-loader'
+            },
+            {
+                loader: 'sass-loader',
+            }
+        ]
+    };
 };
