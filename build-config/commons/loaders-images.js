248ae57d9630c4f96c179f3ad16e8b1073d10da1
Harry Ohlsen <harry.ohlsen@3plearning.com>
2017-09-06 10:12:43 +1000

    Removed image-webpack-loader for the time being

--- build-config/commons/loaders-images.js
+++ build-config/commons/loaders-images.js
@@ -9,35 +9,6 @@ module.exports = function () {
                     outputPath: 'assets/images/',
                     publicPath: process.env.ASSET_CDN_PATH ? process.env.ASSET_CDN_PATH : ''
                 }
-            },
-            {
-                loader: 'image-webpack-loader',
-                options: {
-                    progressive: true,
-                    mozjpeg: {
-                        quality: 65
-                    },
-                    gifsicle: {
-                        interlaced: false
-                    },
-                    optipng: {
-                        optimizationLevel: 7
-                    },
-                    pngquant: {
-                        quality: '65-90',
-                        speed: 4
-                    },
-                    svgo: {
-                        plugins: [
-                            {
-                                removeViewBox: false
-                            },
-                            {
-                                removeEmptyAttrs: false
-                            }
-                        ]
-                    }
-                }
             }
         ]
     };
