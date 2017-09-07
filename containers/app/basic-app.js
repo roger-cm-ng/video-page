fd1eef75a3f8fdfb0583d3d255e6a62d27d69abf
Harry Ohlsen <hohlsen@iinet.net.au>
2017-09-08 07:33:51 +1000

    WIP 2017-09-08 0730

--- containers/app/basic-app.js
+++ containers/app/basic-app.js
@@ -43,9 +43,38 @@ export default class BasicApp {
     }
 
     static configure() {
-        const queryParams = window.location.search;
+        // How we want to configure things depends on where this app is running,
+        // and where the services we want to access live, and whether we want to
+        // mock things.
+        //
+        // 1. When running on a developer's machine, using a mock server
+        //    we want to use a hard-coded config from
+        //
+        //       helpers/local-resource-config.js
+        //
+        // 2. When running on a developer's machine, but we want to use services
+        //    on demo, QA or live, we want to get the configuration from the
+        //    configuration of the host app.
+        //
+        // 3. When running on demo, QA or live, the configuration will be injected
+        //    based on the host app's configuration, augmented by query parameters.
+        //
+        // We determine which situation we have by looking at the query parameters
+        // and whether there is a pppAppConfig property on the window object.
+
+        const location = window.location;
+        const queryParams = location.search;
         const query = queryParams.replace('?', '');
         const params = queryString.parse(query);
+
+        if (/^http:\/\/localhost/.test(location)) {
+            // We are running on a developer's machine
+
+            BasicApp.configureForDeveloperMachine(params);
+        } else {
+            BasicApp.configureForRemoteHost(params);
+        }
+
         const config = window.pppAppConfig;
 
         if (config) {
@@ -53,6 +82,52 @@ export default class BasicApp {
         } else {
             BasicApp.configureUsingQueryParams(params);
         }
+    }
+
+    static configureForDeveloperMachine(params) {
+        const username = params.username;
+        const password = params.password;
+
+        const envName = params.env || 'live';
+
+        const env = Resources.initialise(envName, username, password);
+
+        env.authToken = undefined;
+
+        /* eslint-disable no-console */
+        if (username && password) {
+            console.log(`EntryApp.configureUsingQueryParams() - username = '${username}' password = '${password}'`);
+
+            BasicApp.setCredentials(env, params);
+        } else if (env.needsAuthentication) {
+            console.log('EntryApp.configureUsingQueryParams() - the query parameters do not include a username' +
+                ' and a password.  Perhaps you forgot to pass them in the URL? Alternatively, your application' +
+                ' can call Environment.gatherCredentialsWith() somewhere, passing a function that can be used to' +
+                ' request the username/password.');
+        }
+    }
+
+    static configureForRemoteHost(params) {
+        const config = window.pppAppConfig;
+        const envName = config.env || 'live';
+
+        const env = Resources.initialise(envName);
+
+        env.authToken = config.authToken;
+    }
+
+    static getParameter(name, params, config) {
+        // Extract the specified parameter from either
+        // the query parameters, or the config, with
+        // the former taking precedence.
+
+        let value = params[name];
+
+        if (value === undefined && config) {
+            value = config[name];
+        }
+
+        return value;
     }
 
     static configureUsingAppConfig(config) {
