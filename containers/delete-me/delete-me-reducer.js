da1481e7da9b84e2471ca6bc3e6a301b801c2964
Harry Ohlsen <harry.ohlsen@3plearning.com>
2017-09-08 08:01:57 +1000

    Handle failures when retrieving in the albums example

--- containers/delete-me/delete-me-reducer.js
+++ containers/delete-me/delete-me-reducer.js
@@ -5,9 +5,23 @@ const INITIAL_STATE = { albumTitle: undefined };
 export default function (state = INITIAL_STATE, action) {
     switch (action.type) {
     case GET_ALBUM:
-        return { ...state, albumTitle: action.payload.data.title };
+        return album();
 
     default:
+    }
+
+    function album() {
+        const payload = action.payload;
+        const message = payload.message;
+
+        if (message) {
+            const status = payload.status;
+
+            return { ...state,
+                albumTitle: `Sorry, I failed to retrieve the album; status = ${status} message = ${message}` };
+        }
+
+        return { ...state, albumTitle: payload.data.title };
     }
 
     return state;
