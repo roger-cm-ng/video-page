93764a8a43e853ba8ea72ac5aedb5ff4c6c6f297
richardmurnane3p <richard.murnane@3plearning.com>
2017-09-28 14:15:16 +1000

    Renamed test-helper to kebab case at ESLint's request

--- test/test-helper.js
+++ test/test-helper.js
@@ -8,8 +8,8 @@ import chai, { expect } from 'chai';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import reducers from '../src/reducers';
 import chaiJquery from 'chai-jquery';
+import CombinedReducers from '../containers/app/combined-reducers';
 
 // Set up testing environment to run like a browser in the command line
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
@@ -18,22 +18,23 @@ const $ = jquery(global.window);
 
 // build 'renderComponent' helper that should render a given react class
 function renderComponent(ComponentClass, props, state) {
-  const componentInstance = TestUtils.renderIntoDocument(
-    <Provider store={createStore(reducers, state)}>
-      <ComponentClass {...props} />
-    </Provider>
-  );
+    const componentInstance = TestUtils.renderIntoDocument(
+        <Provider store={createStore(CombinedReducers, state)}>
+            <ComponentClass {...props} />
+        </Provider>
+    );
 
-  return $(ReactDOM.findDOMNode(componentInstance)); // produces HTML
+    /* eslint react/no-find-dom-node: 0 */
+    return $(ReactDOM.findDOMNode(componentInstance)); // produces HTML
 }
 
 // Build helper for simulating events
 $.fn.simulate = function(eventName, value) {
-  if (value) {
-    this.val(value);
-  }
+    if (value) {
+        this.val(value);
+    }
 
-  TestUtils.Simulate[eventName](this[0]);
+    TestUtils.Simulate[eventName](this[0]);
 };
 
 // Set up chai-jquery
